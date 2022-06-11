f7456c2ca483e25e14cfa6a8a088482b0129362d
lathifahkhansa27 <lathifah.khansa.ft20@mail.umy.ac.id>
2022-06-11 22:21:24 +0700

    Add project files.

---
+++ ConnectedNew/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Connected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionstring = GetConnectionString();
+            string query1 = "select * from pembimbing_akademik where NIK = 333";
+            using (SqlConnection cn = new SqlConnection(connectionstring))
+            {
+                SqlCommand cmd1 = new SqlCommand(query1, cn); cn.Open();
+                using (SqlDataReader dr1 = cmd1.ExecuteReader())
+                {
+                    while (dr1.Read())
+                    {
+                        string query2 = "UPDATE Pembimbing_Akademik SET Keahlian = 'Jaringan' WHERE NIK = 333";
+                        SqlCommand cmd2 = new SqlCommand(query2, cn);
+                        cmd2.ExecuteNonQuery();
+                        Console.WriteLine("Data has been updated");
+
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+
+        private static string GetConnectionString()
+        {
+            return "data source = lathifah;database=ProdiTI;MultipleActiveResultSets=True;User ID=sa;Password=kimtaehyung";
+        }
+    }
+}
